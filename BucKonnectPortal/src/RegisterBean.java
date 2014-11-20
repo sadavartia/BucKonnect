c60715ba76a99150301f563cf1f11fa52f5b2129
Nandkumar Khobare <https://nandukhobare@bitbucket.org>
2014-11-20 18:54:00 -0500

    First DB connection try - Nandu

---
+++ BucKonnectPortal/src/RegisterBean.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import bucKonnect.EJB.Entities.*;
+import bucKonnect.EJB.Sessions.*;
+
+/**
+ * 
+ */
+
+/**
+ * @author Nandkumar
+ *
+ */
+public class RegisterBean {
+	@EJB
+	private UserService userService;
+	private String name;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String register_User() {
+		if ((name.isEmpty() != true) && (password.isEmpty() != true)) {
+			UserEntity user = new UserEntity();
+			user.setOSU_Email_Id(getName());
+			user.setPassword(getPassword());
+			String ret = userService.register_User(user);
+			if (!ret.equalsIgnoreCase("success"))
+				return "true";
+			else
+				return "Error";
+		}
+		else
+			return "Error";
+	}
+}
